de11c41df6eddc01cc82a7547f9fbaeb54b23b0c
Yusufjon <qoraboyevyusufjon9@gmail.com>
2026-08-20 21:39:13 +0500

    Update on logic.py and added weak_passwords.txt

--- logic.py
+++ logic.py
@@ -1,44 +1,61 @@
 import re
 
+def check_length(password: str) -> bool:
+    return len(password) >= 8
+
+def check_regex_patterns(password: str) -> dict:
+    return {
+        "has_upper": bool(re.search(r"[A-Z]", password)),
+        "has_lower": bool(re.search(r"[a-z]", password)),
+        "has_digits": bool(re.search(r"\d", password)),
+        "has_special": bool(re.search(r"[!@#$%^&*(),.?\":{}|<>]", password))
+    }
+
+def check_repeated_chars(password: str) -> bool:
+    return bool(re.search(r"(.)\1{2,}", password))
+
+def check_weak_password(password: str, filename: str = 'weak_passwords.txt') -> bool:
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                if password == line.strip():
+                    return True
+
+        return False
+    except FileNotFoundError:
+        return False
+
 def evaluate_password(password: str) -> dict:
-
-    if len(password) < 8:
-        return {
-            "status": "Invalid",
-            "score": 0,
-            "feedback": ["Password should contain at least 8 characters"]
-        }
-
     score = 0
     feedback = []
 
-    if re.search(r"[a-z]", password):
-        score += 1
-    else:
-        feedback.append("Add lowercase letter")
+    if check_weak_password(password, 'weak_passwords.txt'):
+        return {"status": "Weak",
+                "score": 0,
+                "feedback": ["This password is too common and easily guessable!"]}
 
-    if re.search(r"[A-Z]", password):
+    if check_length(password):
         score += 1
     else:
-        feedback.append("Add uppercase letter")
-
-    if re.search(r"\d", password):
-        score += 1
-    else:
-        feedback.append("Add number")
-
-    if re.search(r"[!@#$%^&*()|<>,.?\":]", password):
-        score += 1
-    else:
-        feedback.append("Add a special character")
+        feedback.append("Password should contain at least 8 characters")
 
     if len(password) >= 12:
         score += 1
 
-    if re.search(r"(.)\1{2,}", password):
-        score = max(0, score - 1)
-        feedback.append("Do not use the same characters in a row")
+    patterns = check_regex_patterns(password)
+    for key, value in patterns.items():
+        if value:
+            score += 1
+        else:
+            if key == "has_upper": feedback.append("Add uppercase")
+            if key == "has_lower": feedback.append("Add lowercase")
+            if key == "has_digits": feedback.append("Add digits")
+            if key == "has_special": feedback.append("Add special characters")
 
+    if check_repeated_chars(password):
+        score -= 1
+        feedback.append("Do not use too many of same characters in a row")
+    
     if score <= 2:
         status = "Weak"
     elif score <= 4:
@@ -52,6 +69,6 @@ def evaluate_password(password: str) -> dict:
 user_input = input("Enter the password: ")
 result = evaluate_password(user_input)
 
-print(f"Result: {result['status']} (Score: {result['score']}/5)")
+print(f"Result: {result['status']} (Score: {result['score']}/6)")
 if result['feedback']:
     print("Advices:" , ", ".join(result['feedback']))
